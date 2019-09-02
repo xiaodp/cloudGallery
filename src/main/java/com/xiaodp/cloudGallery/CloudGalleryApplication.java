a7a48d4f3f06c0b31df347527e3dbd72032256bd
xiao_dupi <tjuxdp@163.com>
2019-09-03 00:06:11 +0800

    test

---
+++ src/main/java/com/xiaodp/cloudGallery/CloudGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.xiaodp.cloudGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaodp/cloudGallery/CloudGalleryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaodp.cloudGallery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudGalleryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
